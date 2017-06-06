0abc00c5f856a8b78de533aed863385eef807812
HarshaGUDev <harshagu.gubbi@gmail.com>
2017-06-06 10:26:44 +0530

    accion@people

--- employeeDetails/src/main/resources/static/js/controllers/uploadController.js
+++ employeeDetails/src/main/resources/static/js/controllers/uploadController.js
@@ -76,12 +76,10 @@ app.controller('empDesignation', function($scope, $stateParams, $http,
 	var url = '/accionlabs/emp/details/' + $stateParams.empId + '/designation';
 	orgChart.getListOfEmp(url, function(r) {
 		$scope.empOrgChart = r;
-		console.log($scope.empOrgChart);
-		console.log($scope.empOrgChart[1].length);
-		for (i = 0; i < $scope.empOrgChart[1].length; i++) { 
+	for (i = 0; i < $scope.empOrgChart[1].length; i++) { 
 			$scope.count=$scope.count+$scope.empOrgChart[1][i].empProjectList.length;
 		}
-		console.log($scope.count);
+		
 	});
 	
 
@@ -94,32 +92,37 @@ app.controller('empProject', function($scope, $stateParams, $http, orgChart,
 	$scope.countHigherLevel=0;
 	$scope.countLowerlevel=0;
 	$scope.count=0;
+	$scope.countLowerlevel1=0;
+	$scope.countLowerlevel2=0;
+	$scope.count1=0;
+	$scope.count2=0;
 	var url = '/accionlabs/emp/details/' + $stateParams.empId + '/project';
 	orgChart.getListOfEmp(url, function(r) {
 		$scope.empOrgChart = r;
-		console.log($scope.empOrgChart);
-//		if($scope.empOrgChart<2){
-		$scope.countHigherLevel=$scope.empOrgChart.listOfProjectEmp.length;
+		if($scope.empOrgChart.length!=2){
+			$scope.countHigherLevel=$scope.empOrgChart.listOfProjectEmp.length;
 		for (i = 0; i < $scope.countHigherLevel; i++) { 
 			$scope.countLowerlevel=$scope.countLowerlevel+$scope.empOrgChart.listOfProjectEmp[i].listOfProjectEmp.length;
 		}
-		
 		$scope.count=$scope.countHigherLevel+$scope.countLowerlevel;
-		
-/*		else{
-			$scope.countHigherLevel1=$scope.empOrgChart[0].listOfProjectEmp.length;
-			$scope.countLowerlevel1=0;
+		}
+		if($scope.empOrgChart.length===2){			
+			$scope.countHigherLevel1=$scope.empOrgChart[0].listOfProjectEmp.length;		
 			$scope.countHigherLevel2=$scope.empOrgChart[1].listOfProjectEmp.length;
-			$scope.countLowerlevel2=0;
+		
 				for(i=0;i<$scope.empOrgChart[0].listOfProjectEmp.length;i++){
-					$scope.countLowerlevel1=$scope.countLowerlevel1+empOrgChart[0].listOfProjectEmp[i].listOfProjectEmp.length;
+					$scope.countLowerlevel1=$scope.countLowerlevel1+$scope.empOrgChart[0].listOfProjectEmp[i].listOfProjectEmp.length;		
 				}
-				
+				$scope.count1=$scope.countHigherLevel1+$scope.countLowerlevel1;
+				$scope.count1=$scope.countHigherLevel1+$scope.countLowerlevel1;
 				for(i=0;i<$scope.empOrgChart[1].listOfProjectEmp.length;i++){
-					$scope.countLowerlevel2=$scope.countLowerlevel1+empOrgChart[1].listOfProjectEmp[i].listOfProjectEmp.length;
+					$scope.countLowerlevel2=$scope.countLowerlevel2+$scope.empOrgChart[1].listOfProjectEmp[i].listOfProjectEmp.length;	
+				
 				}
+				$scope.count2=$scope.countHigherLevel2+$scope.countLowerlevel2;
 			
-		}*/
+				}
+	
 	});
 
 });
@@ -132,15 +135,9 @@ app.controller('empReport', function($scope, $stateParams, $http, orgChart,
 	$scope.count=0;
 	var url = '/accionlabs/emp/details/' + $stateParams.empId + '/manager';
 	orgChart.getListOfEmp(url, function(r) {
-		$scope.empOrgChart = r;
-		console.log($scope.empOrgChart);
-		console.log($scope.empOrgChart.length);
-		console.log($scope.empOrgChart[0].length);
+		$scope.empOrgChart = r;	
 		for (i = 0; i < $scope.empOrgChart[0].length; i++) { 
 			$scope.count=$scope.count+$scope.empOrgChart[0][i].empProjectList.length;
 		}
-		console.log($scope.count);
-
 	});
-
 });
